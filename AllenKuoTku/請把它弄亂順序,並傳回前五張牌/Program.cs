e3e90c061a98c02a2ce524281cdcf9ec9db0f9f1
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-02-01 16:34:19 +0800

    int git

---
+++ AllenKuoTku/請把它弄亂順序,並傳回前五張牌/Program.cs
@@ -0,0 +1,35 @@
+﻿// 得到52張牌
+using System;
+using System.Collections.Generic;
+
+string[] suits = new string[] { "S", "H", "D", "C" };
+int[] values = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+List<string> allCards = suits.Join(values, _ => 1, _ => 1, (s, v) => $"{s}{v,2}").ToList();
+// 弄亂傳回其中隨機五張，不能重複。
+int countOfCard = 5;
+
+Random rnd = new Random();
+string[] selectedCards = new string[countOfCard];
+int allCardNum = allCards.Count();
+
+
+
+for (int i = 0; i < countOfCard; i++)
+{
+    int n = list.Count;
+    while (n > 1)
+    {
+        n--;
+        int k = rng.Next(n + 1);
+        T value = list[k];
+        list[k] = list[n];
+        list[n] = value;
+    }
+}
+
+// 顯示選擇的卡
+foreach (string card in selectedCards)
+{
+    Console.WriteLine(card);
+}
+Console.ReadLine();
