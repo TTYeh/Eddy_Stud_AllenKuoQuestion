665e52e239bb389529ba644192bce8976b62bfcf
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-03-26 00:24:18 +0800

    完成題目2

--- 2_顯示指定的文字/Allen_DisplaySpecialWord_Practice/Allen_DisplaySpecialWord_Practice/Program.cs
+++ 2_顯示指定的文字/Allen_DisplaySpecialWord_Practice/Allen_DisplaySpecialWord_Practice/Program.cs
@@ -3,10 +3,10 @@ using SolQuestionLib;
 using System.Drawing;
 
 SolQuestion2 Solution = new SolQuestion2();
-Solution.CreatePoundSign(11); // 有測試-1, 11的邊界條件情況
+Solution.CreatePoundSign(10); // 有測試-1, 11的邊界條件情況
 string ans = Solution.DisplayString();
 Console.WriteLine(ans);
 
 //10:40 開始
 //11:25 結束
-//11:40 Debug完成
+//12:00 Debug完成
