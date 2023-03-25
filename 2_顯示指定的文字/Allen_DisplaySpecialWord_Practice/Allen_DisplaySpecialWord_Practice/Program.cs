4154ca4a650b560d45a1fa95c1785a48fa41ebc8
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-03-26 00:21:11 +0800

    update題目2

---
+++ 2_顯示指定的文字/Allen_DisplaySpecialWord_Practice/Allen_DisplaySpecialWord_Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SolQuestionLib;
+using System.Drawing;
+
+SolQuestion2 Solution = new SolQuestion2();
+Solution.CreatePoundSign(11); // 有測試-1, 11的邊界條件情況
+string ans = Solution.DisplayString();
+Console.WriteLine(ans);
+
+//10:40 開始
+//11:25 結束
+//11:40 Debug完成
---
+++ 2_顯示指定的文字/Allen_DisplaySpecialWord_Practice/SolQuestionLib/SolQuestion2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+
+namespace SolQuestionLib
+{
+    /// <summary>
+    /// 參照題目2截圖
+    /// </summary>
+    public class SolQuestion2
+    {
+        // 註解:看起來眼睛就能解?應該不用寫單元測試?
+        private string _DefaultNotation = "#"; // 產生圖樣
+        private int _UserInputRows = 0; // 產生N列
+        private List<string>? Result ; // 暫存N列結果
+        private string ErrorMsg = string.Empty;
+
+        public void checkUserInputRows(int rows)
+        {
+            ErrorMsg = string.Empty;
+            //  範圍限制只能在1 <= row <=10，否則一律當作0
+            if (rows >= 1 && rows <= 10)
+            {
+                _UserInputRows = rows;
+            }
+            else
+            {
+                // ErrorMsg
+                ErrorMsg = $"您輸入的參數{rows}，不滿足1 <= x <= 10的範圍";
+                _UserInputRows = 0;
+            }
+        }
+        /// <summary>
+        /// 產生井字號的obj
+        /// </summary>
+        /// <param name="_UserInputRows">要產生幾行的正整數</param>
+        public void CreatePoundSign(int row)
+        {
+            this.checkUserInputRows(row);
+            Result = new List<string>();
+            for (int i = 0; i < _UserInputRows; i++)
+            {
+                //第0排0個空白1個字符
+                //第1排1個空白1個字符
+                //第2排2個空白1個字符
+                string NspaceString = new string(' ', i) ;
+                string element = NspaceString + _DefaultNotation + "\n";
+                if (i != _UserInputRows - 1) {
+                    Result.Add(element);
+                } else if(i == _UserInputRows - 1) {
+                    // 若是最後一圈就加上自己的reverse元素，形成對稱
+                    List<string> List2 = new List<string>(Result);
+                    List2.Reverse();
+                    Result.Add(element);
+                    Result.AddRange(List2);
+                }
+            }
+        }
+        public string DisplayString() { 
+            bool isCountMorThanZero = Result.Count > 0 && Result != null;
+            return isCountMorThanZero ? String.Join("", Result.ToArray()) : ErrorMsg;
+
+        }
+    }
+}
